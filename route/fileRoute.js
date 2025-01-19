19cfe2095d2eaae578e14a037105f15c80f7ad17
akash-chaudhary0201 <akashchaudhary0201@gmail.com>
2025-01-19 09:45:23 +0530

    A

--- route/fileRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { uploadPDF } = require("../controller/uploadController");
-const { upload } = require("../middleware/fileUpload");
-
-router.post("/upload", upload.single("pdf"), uploadPDF);
-
-module.exports = router;
